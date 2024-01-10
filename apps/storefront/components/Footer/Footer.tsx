f17ab386610839deaf3d2973491d5bb77aa3b1c5
saranshisatgit <saranshisatgit>
2024-01-10 15:48:19 +0100

    Added Logo and Other Fixes

--- apps/storefront/components/Footer/Footer.tsx
+++ apps/storefront/components/Footer/Footer.tsx
@@ -40,7 +40,7 @@ export function Footer({ className, ...rest }: FooterProps) {
         <div className="flex mb-14 sm:mb-10">
           <Link href={paths.$url()} passHref legacyBehavior>
             <a href="pass" className="hidden sm:inline-block">
-              <div className="mt-px group block h-16 w-28 relative grayscale">
+              <div className="mt-px group block h-48 w-64 relative grayscale">
                 <Image src={imagePath} alt="Saleor logo" layout="fill" />
               </div>
             </a>
